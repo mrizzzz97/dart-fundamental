b6f72cfe0f4d23a079d3f53cfafd5e28d30d0faa
mrizzzz97 <rizkisuryapratama2020@gmail.com>
2026-07-14 10:34:01 +0700

    first commit

---
+++ 1_main.dart
@@ -0,0 +1,5 @@
+main(){
+    print("hello world!");
+    print("hello world!");
+    //copy 1 file ke bawah shift + alt + down
+}
---
+++ 2_comment.dart
@@ -0,0 +1,24 @@
+// Single Comment
+// Digunakan untuk menonaktifkan kode baris atau
+// menandakan sebuah catatan
+
+/**Multiline comment 
+ * biasanya di gunakkan untuk menjelaskan sebuah function / logika
+*/
+
+/// documentation comment [main]
+/// aturan dari documentation comment
+/// 1. ditandai ///
+/// 2. untuk memanggil nama function harus menggunakan []
+/// [age]
+
+void main(List <String> args){
+  // mencetak halo ucup!
+  print('hello ucup!');
+
+  print('object');
+}
+
+int age(){
+  return 17;
+}
